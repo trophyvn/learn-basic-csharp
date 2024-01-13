f7611a3e7f303459837e82d8ba70cfd2b23c4517
Tu Nguyen <nsmartspring@gmail.com>
2024-01-14 02:41:30 +0700

    Add console apps

---
+++ LearnCSharp/LearnCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using SimpleLibrary;
+
+namespace LearnCSharp
+{
+    // Class
+    public class Program
+    {
+        // Main Method: Entry point of the application
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, Developer!");
+
+            // Instantiate a class
+            var greeter = new Greeter();
+            // Call a method
+            string greeting = greeter.GetGreetingMessage("World");
+            Console.WriteLine(greeting);
+
+            // Asynchronous programming
+            string asyncGreeting = await greeter.GetGreetingMessageAsync("Asynchronous World");
+            Console.WriteLine(asyncGreeting);
+        }
+    }
+}
---
+++ LearnCSharp/SimpleLibrary/Greeter.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleLibrary
+{
+    public class Greeter
+    {
+        // Property
+        public string DefaultGreeting { get; set; } = "Hello";
+
+        // Method
+        //CPU Bound
+        public string GetGreetingMessage(string name)
+        {
+            return $"{DefaultGreeting}, {name}!";
+        }
+
+        // Asynchronous method
+        // I/O Bound
+        public async Task<string> GetGreetingMessageAsync(string name)
+        {
+            // Simulate an asynchronous operation
+            await Task.Delay(1000);
+            return $"{DefaultGreeting}, {name}!";
+        }
+    }
+}
